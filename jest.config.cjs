884e390f18dd8061b0cd33e2c93a5a65b698c5a9
arthurmarba <arthurksy@gmail.com>
2025-06-18 15:07:48 -0300

    Add heroicons mocks for Jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -11,13 +11,14 @@ const customJestConfig = {
   testEnvironment: 'jsdom',
   // Se você estiver usando @testing-library/jest-dom, descomente a linha abaixo
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleNameMapper: {
-    // Mapeamentos para mocks e alias (mantemos o que já tínhamos)
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js',
-    '^@/app/(.*)$': '<rootDir>/src/app/$1',
-  },
-};
+    moduleNameMapper: {
+      // Mapeamentos para mocks e alias (mantemos o que já tínhamos)
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+      '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js',
+      '^@/app/(.*)$': '<rootDir>/src/app/$1',
+      '^@heroicons/react/(.*)$': '<rootDir>/__mocks__/heroicons/$1.js',
+    },
+  };
 
 // createJestConfig é exportado desta forma para garantir que o next/jest possa carregar a configuração do Next.js, que é assíncrona
 module.exports = createJestConfig(customJestConfig);
