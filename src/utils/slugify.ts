94eb06427c61d04c66f447fbd3aabb32e6ab686e
arthurmarba <arthurksy@gmail.com>
2025-06-29 19:39:14 -0300

    Fix slugify diacritic removal

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,7 +1,8 @@
 export default function slugify(input: string): string {
   return input
     .normalize('NFD')
-    .replace(/\p{Diacritic}/gu, '')
+    // Remove accents and diacritics
+    .replace(/[\u0300-\u036f]/g, '')
     .toLowerCase()
     .replace(/[^a-z0-9\s-]/g, '')
     .trim()
