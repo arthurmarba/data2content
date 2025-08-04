821525097488fa45f4db41c3cc7b5a9fca299afb
arthurmarba <arthurksy@gmail.com>
2025-08-04 16:52:20 -0300

    refactor: move home data to modules

---
+++ src/types/home.ts
@@ -0,0 +1,13 @@
+export interface FAQItem {
+    q: string;
+    a: string;
+}
+
+export interface Testimonial {
+    name: string;
+    handle: string;
+    quote: string;
+    avatarUrl: string;
+}
+
+export type HeroQuestion = string;
