3593b8614a94e3ea3929e2c157338a09ec74f210
arthurmarba <arthurksy@gmail.com>
2025-08-11 22:52:39 -0300

    fix jest path alias and test env for admin redemptions

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom';
+
+// Polyfills for Next.js API routes during testing
+import { TextEncoder, TextDecoder } from 'util';
+
+// Ensure encoding APIs exist in the test environment
+global.TextEncoder = TextEncoder;
+// @ts-ignore
+global.TextDecoder = TextDecoder;
